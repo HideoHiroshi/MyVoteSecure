a898364b02ffaa6c287396c82670ff1ece5a1543
HideoHiroshi <ahmadmichaelnoob@gmail.com>
2025-11-09 02:15:48 +0800

    Delete MyVoteSecure directory

--- MyVoteSecure/kawalan-pengguna.php
+++
@@ -1,7 +0,0 @@
-<?php
-//hanya membenarkan pengguna berdaftar sahaja yang boleh akses fail
-if(empty($_SESSION['jenis_pengguna']) || $_SESSION['jenis_pengguna'] != "pengundi"){
-    die("<script>alert('sila login'); 
-    window.location.href='logout.php';</script>");
-}
-?>
--- MyVoteSecure/pengguna-data.php
+++
@@ -1,4 +0,0 @@
-4441|azman|123abc|
-5551|azmin|123abc|
-6661|azamn|123abc|
-7771|azmii|123abc|
